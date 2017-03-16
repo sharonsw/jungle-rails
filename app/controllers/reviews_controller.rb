1f97d74cebeec9966ff4ad6f3280e90a5c356572
sharon sw <sharonsuetwli@gmail.com>
2017-03-16 23:29:17 +0000

    Added delete review feature

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -4,5 +4,9 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
+    @product = Product.find(params[:product_id]) 
+    @review = @product.reviews.find(params[:id]) 
+    @review.destroy #remove selected comment in database
+    redirect_to product_path(@product)
   end
 end
