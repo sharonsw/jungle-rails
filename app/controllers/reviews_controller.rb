ec9dfa9fb1ce6b993c64b17a1ca7037beeb79fc9
sharon sw <sharonsuetwli@gmail.com>
2017-03-17 00:03:37 +0000

    Fixed form error and added bootstrap classes to form

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,9 @@
 class ReviewsController < ApplicationController
 
   def create
+    @product = Product.find(params[:product_id])
+    @review = @product.reviews.create(review_params)
+    redirect_to product_path(@product)
   end
 
   def destroy
@@ -8,5 +11,10 @@ class ReviewsController < ApplicationController
     @review = @product.reviews.find(params[:id]) 
     @review.destroy #remove selected comment in database
     redirect_to product_path(@product)
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
